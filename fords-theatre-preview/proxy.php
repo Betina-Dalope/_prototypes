6155c4b4efeac8be4d7266755e6f3ab08c2edbe6
Betina Dalope <betina@bluecadet.com>
2018-07-25 14:17:52 -0400

    try new build

--- fords-theatre-preview/proxy.php
+++ fords-theatre-preview/proxy.php
@@ -4,17 +4,18 @@ header("Access-Control-Allow-Origin:*");
 header('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 header('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
 
-$url = $_GET['url'];
+// $url = $_GET['url'];
 // if(isset($url)) $domainPath = $url;
-$username = 'longwood_dev';
-$password = 'th3s3cr3t0rchid';
+$username = '';
+$password = '';
 
-print_r($url);
- 
-// $context = stream_context_create(array(
-//     'http' => array(
-//         'header'  => "Authorization: Basic " . base64_encode("$username:$password")
-//     )
-// ));
-$string = file_get_contents($url, false);
+$domainPath = "https://www.fords.org/ajax/feeds/schedule/?key=aCmBM8R6ZFBseaJGrec2dsjA4YdZkuQA";
+
+$context = stream_context_create(array(
+    'http' => array(
+        'header'  => "Authorization: Basic " . base64_encode("$username:$password")
+    )
+));
+
+$string = file_get_contents($domainPath, false, $context);
 print_r($string);
