5905e2c8b54c46a12ae8259b6c21f6abb59baed1
Betina Dalope <betina@bluecadet.com>
2018-08-03 14:56:22 -0400

    omg is it a typo thing

--- fords-theatre-preview/proxy.php
+++ fords-theatre-preview/proxy.php
@@ -1,8 +1,8 @@
 <?php 
 header('content-type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin:*");
-header('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-header('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
+header('Access-Control-Allow-Headers, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+header('Access-Control-Allow-Methods, GET,HEAD,OPTIONS,POST,PUT');
 
 
 //$domainPath = "https://www.fords.org/ajax/feeds/schedule/?key=aCmBM8R6ZFBseaJGrec2dsjA4YdZkuQA";
