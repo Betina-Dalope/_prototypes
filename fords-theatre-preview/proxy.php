fe0324eb144b9821a89420c9df1d81a923839705
Betina Dalope <betina@bluecadet.com>
2018-08-03 14:19:59 -0400

    roll back

--- fords-theatre-preview/proxy.php
+++ fords-theatre-preview/proxy.php
@@ -4,17 +4,12 @@ header("Access-Control-Allow-Origin:*");
 header('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 header('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
 
-
-//$domainPath = "https://www.fords.org/ajax/feeds/schedule/?key=aCmBM8R6ZFBseaJGrec2dsjA4YdZkuQA";
-$domainPath = "";
-$url = $_GET['url'];
-
-if(isset($url)) $domainPath = $url;
-
+// $url = $_GET['url'];
+// if(isset($url)) $domainPath = $url;
 $username = '';
 $password = '';
 
-
+$domainPath = "https://www.fords.org/ajax/feeds/schedule/?key=aCmBM8R6ZFBseaJGrec2dsjA4YdZkuQA";
 
 $context = stream_context_create(array(
     'http' => array(
