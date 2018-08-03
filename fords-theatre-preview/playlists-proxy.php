f15cfc11f44c2b5a90eaed9a4ce4eb096502efcf
Betina Dalope <betina@bluecadet.com>
2018-08-03 12:22:55 -0400

    separate proxies

---
+++ fords-theatre-preview/playlists-proxy.php
@@ -0,0 +1,25 @@
+<?php 
+header('content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin:*");
+header('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+header('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
+
+//$domainPath = "";
+//$url = $_GET['url'];
+$domainPath = "http://live-fordstheatre.pantheonsite.io/api/playlists";
+
+$username = '';
+$password = '';
+
+
+
+$context = stream_context_create(array(
+    'http' => array(
+        'header'  => "Authorization: Basic " . base64_encode("$username:$password")
+    )
+));
+
+$string = file_get_contents($domainPath, false, $context);
+print_r($string);
+
+?>
--- fords-theatre-preview/schedule-proxy.php
+++ fords-theatre-preview/schedule-proxy.php
@@ -4,12 +4,9 @@ header("Access-Control-Allow-Origin:*");
 header('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 header('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
 
-
-//$domainPath = "https://www.fords.org/ajax/feeds/schedule/?key=aCmBM8R6ZFBseaJGrec2dsjA4YdZkuQA";
-$domainPath = "";
-$url = $_GET['url'];
-
-if(isset($url)) $domainPath = $url;
+//$domainPath = "";
+//$url = $_GET['url'];
+$domainPath = "https://www.fords.org/ajax/feeds/schedule/?key=aCmBM8R6ZFBseaJGrec2dsjA4YdZkuQA";
 
 $username = '';
 $password = '';
@@ -24,3 +21,5 @@ $context = stream_context_create(array(
 
 $string = file_get_contents($domainPath, false, $context);
 print_r($string);
+
+?>
